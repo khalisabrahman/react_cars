9f9e20e7ebb047c4df9e72a75d5b3deff257ae38
khalisabrahman <khalisabrahman@gmail.com>
2023-05-16 11:59:23 +0800

    carList

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import  CarForm  from "./components/CarForm";
 import  CarList  from "./components/CarList";
 import  CarSearch  from "./components/CarSearch";
 import  CarValue  from "./components/CarValue";
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <CarForm />
       <CarList />
       <CarSearch />
       <CarValue />
-    </div>
+    </Provider>
   );
 }
 
